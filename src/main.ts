bc0c00f1338aa6627337c0ea5cd1f6988143c1b1
weichuanbo8 <wyweichuanbo@163.com>
2024-11-08 11:10:27 +0800

    v0.9 项目初创，尚未完成组建的配置

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+/* 
+    这是一个基于UserManagerWebApi的前端展示系统
+    1、创建项目
+        a、本地gitHub根目录下创建项目
+        b、切换到新建的userManagerWeb目录下执行加载
+        c、加载组件
+        d、配置组件
+    以上内容的具体步骤参考E:\job\document\学习总结.xmind
+*/
+
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+
+createApp(App).mount('#app')
